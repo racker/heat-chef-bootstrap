3557019cae005ffec8a9154e45dda52c48c91fe2
Justin Seubert <dude051@gmail.com>
2015-10-28 12:05:10 -0500

    Initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # heat-chef-bootstrap
 Heat user_data template to perform a Chef bootstrap
+
+# Usage
+Add resource values to your heat template for either `OS::Nova::Server` or some other server creation type such as `Rackspace::AutoScale::Group` in the user_data section.
+
+Example:
+```
+    type: "OS::Nova::Server"
+    properties:
+      name: { get_param: name }
+      flavor: { get_param: flavor }
+      image: { get_param: image }
+      user_data:
+        str_replace:
+          template:
+            get_file: https://raw.githubusercontent.com/racker/heat-chef-bootstrap/master/chef_bootstrap.sh
+          params:
+            "%chef_server_url%": { get_param: chef_server_url }
+            "%chef_version%": { get_param: chef_version }
+            "%chef_organization%": { get_param: chef_organization }
+            "%chef_environment%": { get_param: chef_environment }
+            "%chef_run_list%": { get_param: chef_storage_role }
+            "%chef_validation_key%": { get_param: chef_validation_key }
+            "%chef_encrypted_secret_key%": { get_param: chef_encrypted_secret_key }
+```
+
+# Configuration
+The following parameters should be configured in your template to string replace the script:
+
+Parameter                 | Description
+--------------------------|------------
+chef_server_url           | The URL for your chef server. Default - `https://api.opscode.com/organizations/`
+chef_version              | The chef version to bootstrap the server with. Default: `12.5`
+chef_organization         | Chef server organization to use. Required
+chef_environment          | Chef environment to bootstrap into. Required
+chef_run_list             | Run list of chef recipes or roles to bootstrap application servers. Required
+chef_attributes           | Attributes, in JSON form, to set for chef-client first boot. Optional
+chef_validation_key       | Chef organization validation key. Required
+chef_encrypted_secret_key | A encrypted_data_bag_secret to pass to the server. Optional 
+wc_notify                 | Heat wait condition notification. Optional, Default : `{"status": "SUCCESS"'
---
+++ chef_bootstrap.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+LOGFILE="/var/log/cloud-init-chef-bootstrap.setup.$$"
+# Initial timestamp and debug information
+date > $LOGFILE
+echo "Starting cloud-init bootstrap" >> $LOGFILE
+echo "chef_version parameter: %chef_version%" >> $LOGFILE
+echo "organization parameter: %chef_organization%" >> $LOGFILE
+echo "run list parameter: %chef_run_list%" >> $LOGFILE
+# Infer the Chef Server's URL if none was passed
+CHEFSERVERURL='%chef_server_url%'
+if [ -n $CHEFSERVERURL ]; then
+  echo "chef_server_url parameter: not passed" >> $LOGFILE
+  CHEFSERVERURL="https://api.opscode.com/organizations/%chef_organization%"
+else
+  echo "chef_server_url parameter: $CHEFSERVERURL" >> $LOGFILE
+  CHEFSERVERURL="%chef_server_url%"
+fi
+# Store the validation key in /etc/chef/validator.pem
+echo "Storing validation key in /etc/chef/validator.pem"
+mkdir /etc/chef /var/log/chef &>/dev/null
+cat >/etc/chef/validator.pem <<EOF
+%validation_key%
+EOF
+# Store the encrypted_data_bag_secret if provided
+if [ -n '%chef_encrypted_secret_key%' ]; then
+  echo "Storing data bag secret in /etc/chef/encrypted_data_bag_secret"
+  cat >/etc/chef/encrypted_data_bag_secret <<EOF
+%chef_encrypted_secret_key%
+EOF
+fi
+# Cook a minimal client.rb for getting the chef-client registered
+echo "Creating a minimal /etc/chef/client.rb" >> $LOGFILE
+touch /etc/chef/client.rb
+cat >/etc/chef/client.rb <<EOF
+  log_level        :info
+  log_location     STDOUT
+  chef_server_url  "$CHEFSERVERURL"
+  validation_key         "/etc/chef/validator.pem"
+  validation_client_name "%chef_organization%-validator"
+  environment      "%chef_environment%"
+EOF
+# Cook the first boot file
+echo "Creating a minimal /etc/chef/first-boot.json" >> $LOGFILE
+touch /etc/chef/first-boot.json
+cat >/etc/chef/first-boot.json <<EOF
+{"run_list":["%chef_run_list%"]
+EOF
+if [ -n '%chef_attributes%' ]; then
+  cat >>/etc/chef/first-boot.json <<EOF
+,
+%chef_attributes%
+EOF
+fi
+cat >>/etc/chef/first-boot.json <<EOF
+}
+EOF
+# Install chef-client through omnibus (if not already available)
+if [ ! -f /usr/bin/chef-client ]; then
+  echo "Installing chef using omnibus installer" >> $LOGFILE
+  # adjust to install the latest vs. a particular version
+  curl -L https://www.opscode.com/chef/install.sh | bash -s -- -v %chef_version% &>$LOGFILE
+  echo "Installation of chef complete" >> $LOGFILE
+else
+  echo "Existing chef found and is being used" >> $LOGFILE
+  echo "Existing chef version: $(chef-client --version)" >> $LOGFILE
+fi
+# Kick off the first chef run
+echo "Executing the first chef-client run"
+if [ -f /usr/bin/chef-client ]; then
+  echo "First Chef client run" >> $LOGFILE
+  /usr/bin/chef-client -j /etc/chef/first-boot.json
+fi
+# Script complete. Log final timestamp
+date >> $LOGFILE
+wc_notify --data-binary '{"status": "SUCCESS"}'
